bc2bb2830d2d99bdca435686f61c8ddb4c6d0501
k18rs139 <k18rs139@st.kyusan-u.ac.jp>
2019-11-25 13:55:39 +0900

    ファイルごとに追加

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,6 +1,21 @@
 // This is a JavaScript file
 // ncmbController.js
 
+
+
 var ncmbController = {
+    APPLICATION_KEY: "YOUR_APPLICATION_KEY",
+    CLIENT_KEY: "YOUR_CLIENT_KEY",
+
+    ncmb: null,
+    currentUser: null,  // ログインしたユーザーのオブジェクトを格納
+    screenSize: null,    // 画面サイズを格納
+
+    // 初期化
+    init: function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+    }
 }
 
